81155e69f861f84c58bed36da50fef36721b83fa
Amar Latthe <amarlatthe060@gmail.com>
2019-08-29 15:20:26 +0530

    initial_commit

---
+++ src/main/java/com/logicoy/monitor/cal/CalculatorImpl.java
@@ -0,0 +1,37 @@
+package com.logicoy.monitor.cal;
+
+/**
+ * 
+ * @author Amar
+ *
+ */
+public class CalculatorImpl implements Calculator {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplication(int a, int b) {
+		return a * b;
+	}
+
+	public int division(int a, int b) throws Exception {
+		if(b == 0) {
+			throw new Exception("Divider can't be zero");
+		}
+		return a / b;
+	}
+
+	public boolean equalIntegers(int a, int b) {
+		boolean result = false;
+		if(a == b) {
+			return true;
+		}
+		return result;
+	}
+	
+}
